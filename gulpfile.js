353389b21f24e75f0d056012c53301c78929216b
mattaxell <hello@mattaxell.com>
2014-11-02 21:13:00 +0000

    Update gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
 // Dependencies
+// ============
+
 var gulp = require('gulp'),
+
     // CSS stuff
     sass = require('gulp-sass'),
     autoprefix = require('gulp-autoprefixer'),
     minify = require('gulp-minify-css'),
+
     // Javascript stuff
     uglify = require('gulp-uglify'),
     strip = require('gulp-strip-debug'),
     concat = require('gulp-concat'),
+
     // Images
     imagemin = require('gulp-imagemin'),
     cache = require('gulp-cache'),
+
     // Other
     util = require('gulp-util'),
     clean = require('gulp-clean'),
-    notify = require('gulp-notify'),
-    // Live reload
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
+    notify = require('gulp-notify');
 
 // Assets
+// ======
+
 var paths = {
     assets: {
         styles: {
@@ -34,11 +38,10 @@ var paths = {
         js: {
             dir: 'assets/js/',
             files: [
-                'assets/js/src/**/*.js',
                 'assets/js/vendor/**/*.js',
+                'assets/js/src/**/*.js',
                 'assets/js/main.js'
             ],
-            filesToWatch: 'assets/js/src/**/*.js',
         },
         img: {
             dir: 'assets/img',
@@ -52,20 +55,19 @@ var paths = {
     }
 }
 
-// General settings
+// General Settings
+// ================
 var settings = {
     autoprefix: {
         versions: 'last 10 version'
     }
 }
 
-//
 // Styles task
-// -----------------
+// ===========
 // Grabs everything inside the styles & sprites directories, concantinates
 // and compiles scss, builds sprites, and then outputs them to their
 // respective target directories.
-//
 
 gulp.task('styles', function() {
     gulp.src(paths.assets.styles.files)
@@ -75,9 +77,8 @@ gulp.task('styles', function() {
         .pipe(notify('Styles task complete.'));
 });
 
-//
 // Login styles task
-// -----------------
+// =================
 
 gulp.task('login-styles', function() {
     gulp.src(paths.assets.styles.login)
@@ -87,12 +88,10 @@ gulp.task('login-styles', function() {
         .pipe(notify('Styles task complete.'));
 });
 
-//
 // Javascript task
-// ---------------
+// ===============
 // Grabs everything inside the js directory, concantinates and minifies,
 // and then outputs them to the target directory.
-//
 
 gulp.task('js', function() {
     gulp.src(paths.assets.js.files)
@@ -101,12 +100,10 @@ gulp.task('js', function() {
         .pipe(notify('JS task complete.'));
 });
 
-//
 // Image task
-// ----------
+// ==========
 // Grabs everything inside the img directory, optimises each image,
 // and then outputs them to the target directory.
-//
 
 gulp.task('img', function() {
     gulp.src(paths.assets.img.files)
@@ -114,21 +111,17 @@ gulp.task('img', function() {
         .pipe(gulp.dest(paths.public.img));
 });
 
-//
 // Cache-buster task
-// -----------------
+// =================
 // Completely clear the cache to stop image-min outputting oncorrect image names etc
-//
 
 gulp.task('clear', function (done) {
     return cache.clearAll(done);
 });
 
-//
 // Cleaner task
-// ------------
+// ============
 // This simply deletes all of the main assets folders.
-//
 
 gulp.task('clean', function() {
   return gulp.src([paths.public.styles, paths.public.js, paths.public.img], {read: false})
@@ -136,30 +129,21 @@ gulp.task('clean', function() {
     .pipe(notify('Cleaning task complete.'));
 });
 
-//
 // Watch task
-// ----------
+// ==========
 // Watches the different directores for changes and then
 // runs their relevant tasks and livereloads.
-//
 
 gulp.task('watch', function() {
     // Run the appropriate task when assets change
     gulp.watch(paths.assets.styles.files, ['styles']);
     gulp.watch(paths.assets.js.files, ['js']);
     gulp.watch(paths.assets.img.files, ['img']);
-    var server = livereload();
-    // Refresh the browser when anything changes
-    gulp.watch('*').on('change', function(file) {
-        server.changed(file.path);
-    });
 });
 
-//
 // Deploy task
-// -----------
+// ===========
 // Runs all of the main tasks, without activating livereload.
-//
 
 gulp.task('deploy', ['clean'], function() {
     // Run the styles task, but minify the output
@@ -189,9 +173,8 @@ gulp.task('deploy', ['clean'], function() {
 
 //
 // Default task
-// ------------
+// ============
 // Runs every task, and then watches files for changes.
-//
 
 gulp.task('default', ['clean'], function() {
     gulp.start('styles', 'js', 'img', 'watch');
